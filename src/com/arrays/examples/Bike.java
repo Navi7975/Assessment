692b2eece02745da7ebd45b0490e23b367db75ec
navee <navee@Naveen>
2023-11-27 19:20:10 +0530

    First commit

--- src/com/arrays/examples/Bike.java
+++ src/com/arrays/examples/Bike.java
@@ -1,36 +1,37 @@
 package com.arrays.examples;
 
+//Interface for Bike
 interface Bike {
-    void speedUp(int increment); // Method to increase speed
-    void applyBrakes(int decrement); // Method to apply brakes
-    void displaySpeed(); // Method to display speed
+void speedUp(int increment); // Method to increase speed
+void applyBrakes(int decrement); // Method to apply brakes
+String getSpeed(); // Method to get current speed as a string
 }
 
-// Class implementing Bike interface
+//Class implementing Bike interface
 class MountainBike implements Bike {
-    private int speed; // Private speed variable for MountainBike
+private int speed; // Private speed variable for MountainBike
 
-    // Constructor to initialize speed to 0
-    public MountainBike() {
-        speed = 0;
-    }
+// Constructor to initialize speed to 0
+public MountainBike() {
+   speed = 0;
+}
 
-    // Method to increase speed by a specified increment
-    public void speedUp(int increment) {
-        speed += increment;
-    }
+// Method to increase speed by a specified increment
+public void speedUp(int increment) {
+   speed += increment;
+}
 
-    // Method to apply brakes by a specified decrement
-    public void applyBrakes(int decrement) {
-        if (speed - decrement >= 0) {
-            speed -= decrement;
-        } else {
-            speed = 0;
-        }
-    }
+// Method to apply brakes by a specified decrement
+public void applyBrakes(int decrement) {
+   if (speed - decrement >= 0) {
+       speed -= decrement;
+   } else {
+       speed = 0;
+   }
+}
 
-    // Method to display current speed
-    public void displaySpeed() {
-        System.out.println("Current Speed: " + speed + " mph");
-    }
+// Method to get current speed as a string
+public String getSpeed() {
+   return "Current Speed: " + speed + " mph";
+}
 }
